d8416843d4698026ec3506e244ea54cb90c6bfc2
LiborZelenka <zelenli1@fel.cvut.cz>
2025-12-18 21:29:32 +0100

    f4

--- calibration.py
+++ calibration.py
@@ -2,38 +2,41 @@ from ctu_crs import CRS97
 import kinematics as k
 import numpy as np
 
-homography = []
-images = []
-positions = []
 
-def capture_calibration_images():
-    robot = CRS97()
-    robot.initialize()
-    q0 = robot.q_home
-    robot.soft_home()
+class Homography:
+    def __init__(self):
+        self.H = []
+        self.position = []
+        self.images = []
+        self.robot = CRS97()
+        self.robot.initialize()
+        self.robot.soft_home()
 
-    for x in np.arange(0.35, 0.55, 0.05):
-        for y in np.arange(-0.20, 0.20, 0.05):
-                current_pose = k.fk(robot.get_q(), robot)
-                target_pose = current_pose.copy()
-                target_pose[0, 3] = x 
-                target_pose[1, 3] = y
-                target_pose[2, 3] = 0.05
-                target_pose[:3, :3] = k.get_rotation_from_z(np.array([0, 0, -1]))
 
-                ik_sols = k.ik(target_pose, robot)
+    def capture_calibration_images(self):
 
-                if len(ik_sols) > 0:
-                    closest_solution = min(ik_sols, key=lambda q: np.linalg.norm(q - q0))
-                    robot.move_to_q(closest_solution)
-                    robot.wait_for_motion_stop()
+        for x in np.arange(0.35, 0.55, 0.05):
+            for y in np.arange(-0.20, 0.20, 0.05):
+                    current_pose = k.fk(self.robot.get_q(), self.robot)
+                    target_pose = current_pose.copy()
+                    target_pose[0, 3] = x 
+                    target_pose[1, 3] = y
+                    target_pose[2, 3] = 0.05
+                    target_pose[:3, :3] = k.get_rotation_from_z(np.array([0, 0, -1]))
 
-                    images.append(robot.grab_image("calib_img_x{:.2f}_y{:.2f}.png".format(x, y)))
-                    positions.append([x, y, 0.05])
+                    ik_sols = k.ik(target_pose, self.robot)
 
-                else:
-                    print(f"No IK solutions found for target offset ({x}, {y}, {z})!")
-                    continue
+                    if len(ik_sols) > 0:
+                        closest_solution = min(ik_sols, key=lambda q: np.linalg.norm(q - self.robot.get_q()))
+                        self.robot.move_to_q(closest_solution)
+                        self.robot.wait_for_motion_stop()
+
+                        self.images.append(self.robot.grab_image("calib_img_x{:.2f}_y{:.2f}.png".format(x, y)))
+                        self.position.append([x, y, 0.05])
+
+                    else:
+                        print(f"No IK solutions found for target offset ({x}, {y}, {z})!")
+                        continue
 
 
 
